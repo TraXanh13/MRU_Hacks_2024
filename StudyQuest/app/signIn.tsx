10e13b8ff7482548ee19e1e16b70bda05d1856f2
Keen Bullecer <59938709+kbull136@users.noreply.github.com>
2024-10-05 18:13:53 -0600

    initialized app and imported some assets

---
+++ StudyQuest/app/signIn.tsx
@@ -0,0 +1,22 @@
+import {Text, View, StyleSheet} from 'react-native';
+
+export default function SignInScreen() {
+    return (
+        <View style={signInStyle.container}>
+            <Text style={signInStyle.text}>This is a test</Text>
+        </View>
+    )
+}
+
+const signInStyle = StyleSheet.create( {
+    container: {
+        flex: 1,
+        backgroundColor: 'grey',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: 'red',
+    },
+});
+
